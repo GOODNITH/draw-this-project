1d5c5afb99a9a2ff68d90248900f371511a59d89
DrawThis <drawthis6@gmail.com>
2024-10-16 00:10:30 -0500

    Conexión a la base de datos

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,3 +5,12 @@ export const actions = {
         redirect(302, '/landing-page')
     }
 }
+
+import { db } from '$lib/server/database/connection';
+import { users } from '$lib/server/database/data';
+
+export async function load() {
+    const usuarios = await db.select().from(users);
+    console.log(usuarios)
+    return { usuarios };
+}
